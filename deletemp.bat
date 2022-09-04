05f75f24ae3b89f7eed21970003c97f849285c79
root <root@vmi755384.contaboserver.net>
2022-09-04 02:56:35 +0200

    deletem

--- deletemp.bat
+++ deletemp.bat
@@ -1,11 +1,11 @@
 echo "stops docker"
-sudo docker stop postgres
-sudo docker stop odoo_instance_11
+sudo docker stop db10
+sudo docker stop fe_odoo
 sudo docker ps
 echo "delete temps"
-echo "" > $(docker inspect --format='{{.LogPath}}' postgres)
-echo "" > $(docker inspect --format='{{.LogPath}}' odoo_instance_11)
+echo "" > $(docker inspect --format='{{.LogPath}}' db10)
+echo "" > $(docker inspect --format='{{.LogPath}}' fe_odoo)
 echo "start dockers"
-sudo docker start postgres
-sudo docker start odoo_instance_11
+sudo docker start db10
+sudo docker start fe_odoo
 sudo docker ps
