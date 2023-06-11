18bf4e38c69f9d7d5196a50bf8866d85956e319e
HP <odooperuerp@gmail.com>
2023-06-10 22:41:22 -0500

    ok

--- c_requirements2/models/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class product2(models.Model):
-	_inherit = 'product.template'
-	estado = fields.Selection([('Disponible','DISPONIBLE'),('Vendido','VENDIDO'),('Separado','SEPARADO')],string="Estado")
-	
